e922b8df5f25eada2ce7c31f99c4f7ae31e8e1a1
Luz199785 <111939240+Luz199785@users.noreply.github.com>
2022-09-27 20:03:09 -0500

    Update cajero.c

--- cajero.c
+++ cajero.c
@@ -115,3 +115,19 @@ void balance ()
     
     printf("\nRs:%d is successfully")
 }
+
+
+//Ejemplo punteros
+int main()
+{
+   int a, b;
+   int c = 5;
+   int *p;
+   
+   a = 4* (c + 5);
+   
+   p = &c;
+   b = 4 * (*p + 5);
+   printf ("a=%d b=%d", a, b);
+}
+
